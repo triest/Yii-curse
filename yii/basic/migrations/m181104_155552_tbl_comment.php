b898f0fc1d12eb6cb939c242fa41bd4ee1f878f6
triest <you@example.com>
2018-11-04 18:56:39 +0300

    create empty migrations files

---
+++ yii/basic/migrations/m181104_155552_tbl_comment.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181104_155552_tbl_comment
+ */
+class m181104_155552_tbl_comment extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m181104_155552_tbl_comment cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m181104_155552_tbl_comment cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
