be19ea15be364761fd713a1fe09c0686bf56a60e
triest <you@example.com>
2018-11-05 11:45:26 +0300

    add relationships betwin table updated

--- yii/basic/migrations/m170124_021622_create_comment_table.php
+++ yii/basic/migrations/m170124_021622_create_comment_table.php
@@ -12,7 +12,7 @@ class m170124_021622_create_comment_table extends Migration
      */
     public function up()
     {
-        $this->createTable('post', [
+        $this->createTable('article', [
             'id' => $this->primaryKey(),
             'text'=>$this->string(),
             'user_id'=>$this->integer(),
@@ -22,15 +22,15 @@ class m170124_021622_create_comment_table extends Migration
 
         // creates index for column `user_id`
         $this->createIndex(
-            'idx-post-user_id',
-            'post',
+            'idx-article-user_id',
+            'article',
             'user_id'
         );
 
         // add foreign key for table `user`
         $this->addForeignKey(
             'fk-article-user_id',
-            'post',
+            'article',
             'user_id',
             'user',
             'id',
